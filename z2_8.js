7dcde4e39fc2dcf56758274eb9d6d0b3fec64a5f
Margarita <72250478+abaturova@users.noreply.github.com>
2020-10-06 15:32:26 +0300

    Update z2_8.js

--- z2_8.js
+++ z2_8.js
@@ -1,6 +1,7 @@
 let weekday = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье']
 let nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-
+let d = 0
+let tab , a
 document.write("<table border='1'>")
 
 document.write("<tr>")
@@ -9,18 +10,24 @@ for(let w=0;w<weekday.length;w++){
 		document.write(weekday[w])
 		document.write("</th>")
 	}
-document.write("</tr>")
+document.write("</tr><tr>")
 
-for(let i=0;i<5;i++){
-	document.write("<tr>")
-	for(let j=0;j<7;j++){
-			document.write("<td>")
-			for(let n=0;n<nums.length;n++){					
-					document.write(nums[n])
-			}
-			document.write("</td>")				
+while(d<nums.length){
+	a = nums[d]
+	if(d%7 === 0){
+		for(let i=0;i<7;i++){
+			
+			if(a <= 31){
+				tab += '<td>' + a + '</td>'
+				a++
+			}else tab += '<td> </td>'
+		}
+	}else if (d%7 === 6){
+			tab += '</tr><tr>'
 	}
-	document.write("</tr>")
+	console.log(a)
+	d++	
 }
+document.write(tab)
 
 document.write("</table>")
